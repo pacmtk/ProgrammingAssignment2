c3d0ca9682c37b3bb3a69ec4ea57af034cfe06d2
pacmtk <pacmtk@users.noreply.github.com>
2015-01-24 22:12:08 -0700

    updated with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take advantage of lexical scoping to cache the value of
+## the inverse of a matrix rather than re-computing it every time it is required.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object than can cache its inverse.
 
-## makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-## cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-## another attempt
-makeVector <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
-        print(environment())
-        evn <- environment()
-        print(parent.env(evn))
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        getevn<- function() environment()
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean,
-             getevn = getevn)
+        get <- function () x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get, setinverse = setinverse, 
+             getinverse = getinverse)
+        
+
+}
+
+
+## cacheSolve returns the cached inverse matrix and a message "getting
+## cached data"
+
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
