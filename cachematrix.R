c9b06178820d4874bc89bd37a96375f953a0c916
pacmtk <pacmtk@users.noreply.github.com>
2015-01-24 22:04:44 -0700

    code

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+## cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
 ## another attempt
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        print(environment())
+        evn <- environment()
+        print(parent.env(evn))
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        getevn<- function() environment()
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean,
+             getevn = getevn)
+}
